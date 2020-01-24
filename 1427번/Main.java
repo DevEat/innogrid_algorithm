aab24f434170d37438a77a010840b8ae9266843c
yzzeee <goooooodgood@gmail.com>
2020-01-24 14:32:13 +0900

    소트인사이드 104ms
    - 소트인사이드 104ms

---
+++ 1427번/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        char[] chs = input.toCharArray();
+
+        for (int i = 0; i < chs.length - 1; i++) {
+            for(int j = i; j < chs.length ; j++){
+                if(Character.getNumericValue(chs[i]) < Character.getNumericValue(chs[j])){
+                    char temp = chs[i];
+                    chs[i] = chs[j];
+                    chs[j] = temp;
+                }
+            }
+        }
+
+        System.out.println(String.valueOf(chs));
+
+        scanner.close();
+    }
+}
